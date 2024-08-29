ded4317504135aafb7657587f1404f7e355e0afd
Andres Gaviria <Andres.Gaviria@sleepgroupsolutions.com>
2024-08-29 11:01:15 -0500

    Clase 29-08-2024

--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ea07e7a76ac5d5ec0306132cdc91cb97da6ca1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbda28ce6d33d235bc895d0240bdb425e65ccd78")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
